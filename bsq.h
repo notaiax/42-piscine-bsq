84ff61eba479502e286a464820bec04dfe889db9
Àiax  Faura vilalta <afaura-v@e1z2r8p21.42.us.org>
2019-07-29 19:32:08 -0700

    header file added

---
+++ bsq.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsq.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afaura-v <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/29 18:07:49 by afaura-v          #+#    #+#             */
+/*   Updated: 2019/07/29 19:31:13 by afaura-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BSQ_H
+# define BSQ_H
+
+typedef struct	s_map_info
+{
+	int		lines;
+	int		columns;
+	char	empty;
+	char	obstacle;
+	char	full;
+}				t_map_info;
+
+int				ft_strlen(char *str);
+
+
+
+#endif
--- main.c
+++ main.c
@@ -6,11 +6,47 @@
 /*   By: am <marvin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 17:23:35 by am                #+#    #+#             */
-/*   Updated: 2019/07/29 18:05:26 by migriver         ###   ########.fr       */
+/*   Updated: 2019/07/29 19:31:05 by afaura-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "bsq.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define BUF_SIZE 4096
+
+char	*read_file(int argc, char argv**)
 {
+	int 	fd;
+	char	*buf;
+	int 	ret;
+
+
+	fd = 0;
+	if (argc == 2)
+		fd = open(argv[1], O_RDONLY)
+		if (fd == -1)
+		{
+			write(2, "open() error", 12);
+			return (1);
+		}
+	ret = read(fd, buf, BUF_SIZE);
+	if (ret == -1)
+	{
+		write(2, "read() error", 12);
+		return (1);
+	}
+	
+
+}
+
+int		main(int argc, char argv**)
+{
+	char	*str;
+
+
 	return (0);
 }
