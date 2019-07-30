49f913845b4cb37517cb862288cbf9cf1122f4f7
Aiax Faura Vilalta <aiax.developer@gmail.com>
2019-07-30 00:07:03 -0700

    Change main

--- main.c
+++ main.c
@@ -3,17 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afaura-v <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: am <marvin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/29 17:23:35 by afaura-v          #+#    #+#             */
-/*   Updated: 2019/07/29 23:28:51 by migriver         ###   ########.fr       */
+/*   Created: 2019/07/29 17:23:35 by am                #+#    #+#             */
+/*   Updated: 2019/07/29 23:28:17 by afaura-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <bsq.h>
+#include "bsq.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define BUF_SIZE 4096
 
-int	main(void)
+char	*read_file(int argc, char **argv)
 {
-	ft_putstr("hello, word");
+	int 	fd;
+	char	*buf;
+	int 	ret;
+
+	fd = 0;
+	buf = (char*)malloc(sizeof(*buf) * (BUF_SIZE + 1));
+	if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			write(2, "open() error", 12);
+			return ("");
+		}
+	ret = read(fd, buf, BUF_SIZE);
+	if (ret == -1)
+	{
+		write(2, "read() error", 12);
+		return ("");
+	}
+	buf[ret] = '\0';
+	if (close(fd) == -1)
+	{
+		write(2, "close() error", 13);
+		return ("");
+	}
+	return (buf);
+}
+
+int		main(int argc, char **argv)
+{
+	char	*str;
+	
+	str = read_file(argc, argv);
+	ft_putstr(str);
+
 	return (0);
 }
