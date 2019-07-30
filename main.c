2a785d4acc239adc930120998145c6950da68b53
Aiax Faura Vilalta <aiax.developer@gmail.com>
2019-07-29 20:39:30 -0700

    main modified

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: am <marvin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 17:23:35 by am                #+#    #+#             */
-/*   Updated: 2019/07/29 19:31:05 by afaura-v         ###   ########.fr       */
+/*   Updated: 2019/07/29 20:33:55 by afaura-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@
 #include <stdlib.h>
 #define BUF_SIZE 4096
 
-char	*read_file(int argc, char argv**)
+char	*read_file(int argc, char **argv)
 {
 	int 	fd;
 	char	*buf;
 	int 	ret;
 
-
 	fd = 0;
+	buf = (char*)malloc(sizeof(*buf) * (BUF_SIZE + 1));
 	if (argc == 2)
 		fd = open(argv[1], O_RDONLY)
 		if (fd == -1)
@@ -39,14 +39,21 @@ char	*read_file(int argc, char argv**)
 		write(2, "read() error", 12);
 		return (1);
 	}
-	
-
+	buf[ret] = '\0';
+	if (close(fd) == -1)
+	{
+		write(2, "close() error", 13);
+		return (1);
+	}
+	return (buf);
 }
 
 int		main(int argc, char argv**)
 {
 	char	*str;
-
+	
+	str = read_file(argc, argv);
+	putstr(str);
 
 	return (0);
 }
