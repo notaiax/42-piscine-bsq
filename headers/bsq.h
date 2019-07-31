1a5e7e8986f0bdc49d264f07f540fd7644de0b77
Aiax Faura Vilalta <aiax.developer@gmail.com>
2019-07-30 19:42:35 -0700

    Add box and obstacle structs

--- headers/bsq.h
+++ headers/bsq.h
@@ -6,7 +6,7 @@
 /*   By: afaura-v <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 18:07:49 by afaura-v          #+#    #+#             */
-/*   Updated: 2019/07/30 19:07:31 by afaura-v         ###   ########.fr       */
+/*   Updated: 2019/07/30 19:40:34 by afaura-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,19 @@ typedef struct	s_map_info
 	char	full;
 }				t_map_info;
 
+typedef struct s_obstacle
+{
+	int 				x;
+	int 				y;
+	struct s_obstacle	*next;
+}				t_obstacle;
+
+typedef struct	s_box
+{
+	int x;
+	int y;
+	int size;
+}				t_box;
 
 void	ft_putstr(char *str);
 char	*read_file(int argc, char **argv);
